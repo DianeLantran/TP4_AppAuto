2c854233cd20a7a3203058899d071bf1aabd7df9
DianeLantrann <diane.lantran@gmail.com>
2023-11-16 15:24:25 -0500

    clean 2e csv nul

--- src/main.py
+++ src/main.py
@@ -3,19 +3,17 @@ import preprocessing as prep
 import clustering
 
 # Importation des bases de donnée
-FILE_PATH1 = "../data/unpopular_songs.csv"
-df1 = pd.read_csv(FILE_PATH1, sep=',')
+FILE_PATH = "../data/unpopular_songs.csv"
+df = pd.read_csv(FILE_PATH, sep=',')
 
-FILE_PATH2 = "../data/z_genre_of_artists.csv"
-df2 = pd.read_csv(FILE_PATH2, sep=',')
 
 # Nettoyage des données : 
 
 
 # Preprocessing
-features = df1.columns.difference(["track_name", "track_id"])
+features = df.columns.difference(["track_name", "track_id"])
 categorical_cols = ['track_artist', "explicit"]
 
 # Preprocessing
-df = prep.preprocess(df1, categorical_cols, features)
+df = prep.preprocess(df, categorical_cols, features)
 
