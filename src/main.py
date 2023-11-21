7c8cb8efc2bb2db5e1252958757ee058c897ac7b
Courbayre Basil <basil.courbayre@ecole.ensicaen.fr>
2023-11-21 00:49:22 -0500

    var name change

--- src/main.py
+++ src/main.py
@@ -4,13 +4,13 @@ import clustering
 
 # Importation des bases de donnée
 FILE_PATH1 = "../data/genres_v2.csv"
-df1 = pd.read_csv(FILE_PATH1, sep=',')
+df = pd.read_csv(FILE_PATH1, sep=',')
 
 # Nettoyage des données : 
 
 
 # Preprocessing
-features = df1.columns.difference(["type", "id", "uri", "track_href", 
+features = df.columns.difference(["type", "id", "uri", "track_href", 
                                    "analysis_url", "song_name",
                                    "Unnamed: 0", "title", "time_signature",
                                    "duration_ms"])
